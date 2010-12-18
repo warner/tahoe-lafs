6559cb243dd0dc0344541a642511aba888e071ac
Brian Warner <warner@lothar.com>
2010-12-18 11:49:46 -0800

    doodling

---
+++ docs/webapp/samples/counter/main.js
@@ -0,0 +1,8 @@
+document.innerHTML = '<div>Counter: <span id="counter">?</span>'
+                   + '<input type="button" value="Count" onclick="count();"/>'
+                   + '</div>';
+var counter = 0;
+function count() {
+    counter += 1;
+    document.getElementById("counter").HTML='<span id="counter">'+counter+'</span>';
+}
