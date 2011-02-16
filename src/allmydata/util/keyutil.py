6545b8b20fb4efeb62f7a24097c73c7037964cf0
Brian Warner <warner@lothar.com>
2011-02-15 18:06:07 -0800

    switch to NIST256p, since 224 is too short

--- src/allmydata/util/keyutil.py
+++ src/allmydata/util/keyutil.py
@@ -1,18 +1,18 @@
-from allmydata.util.ecdsa import SigningKey, VerifyingKey, NIST192p
+from allmydata.util.ecdsa import SigningKey, VerifyingKey, NIST256p
 from allmydata.util import base32
 
-# in base32, the signing key is 39 chars long, and the verifying key is 77.
-# in base62, the signing key is 33 chars long, and the verifying key is 65.
-# in base64, the signing key is 32 chars long, and the verifying key is 64.
+# in base32, the signing key is 52 chars long, and the verifying key is 103.
+# in base62, the signing key is 43 chars long, and the verifying key is 86.
+# in base64, the signing key is 43 chars long, and the verifying key is 86.
 #
 # We can't use base64 because we want to reserve punctuation and preserve
-# cut-and-pasteability. The base62 encoding is not significantly shorter than
-# the base32 form, and the minor usability improvement is not worth the
-# documentation/specification confusion of using a non-standard encoding. So
-# we stick with base32.
+# cut-and-pasteability. The base62 encoding is shorter than the base32 form,
+# but the minor usability improvement is not worth the documentation and
+# specification confusion of using a non-standard encoding. So we stick with
+# base32.
 
 def make_keypair():
-    privkey = SigningKey.generate(curve=NIST192p)
+    privkey = SigningKey.generate(curve=NIST256p)
     privkey_vs = "priv-v0-%s" % base32.b2a(privkey.to_string())
     pubkey = privkey.get_verifying_key()
     pubkey_vs = "pub-v0-%s" % base32.b2a(pubkey.to_string())
@@ -22,7 +22,7 @@ def parse_privkey(privkey_vs):
     if not privkey_vs.startswith("priv-v0-"):
         raise ValueError("private key must look like 'priv-v0-...', not '%s'" % privkey_vs)
     privkey_s = privkey_vs[len("priv-v0-"):]
-    sk = SigningKey.from_string(base32.a2b(privkey_s), curve=NIST192p)
+    sk = SigningKey.from_string(base32.a2b(privkey_s), curve=NIST256p)
     pubkey = sk.get_verifying_key()
     pubkey_vs = "pub-v0-%s" % base32.b2a(pubkey.to_string())
     return sk, pubkey_vs
@@ -31,5 +31,5 @@ def parse_pubkey(pubkey_vs):
     if not pubkey_vs.startswith("pub-v0-"):
         raise ValueError("public key must look like 'pub-v0-...', not '%s'" % pubkey_vs)
     pubkey_s = pubkey_vs[len("pub-v0-"):]
-    vk = VerifyingKey.from_string(base32.a2b(pubkey_s), curve=NIST192p)
+    vk = VerifyingKey.from_string(base32.a2b(pubkey_s), curve=NIST256p)
     return vk
