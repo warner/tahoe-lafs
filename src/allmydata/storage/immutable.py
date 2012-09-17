088a3d3951d404c4dd556ccabd5e64b0d7633728
David-Sarah Hopwood <david-sarah@jacaranda.org>
2012-09-17 18:32:29 +0100

    Delete unused lease handling code from share classes.
    Signed-off-by: David-Sarah Hopwood <david-sarah@jacaranda.org>

--- src/allmydata/storage/immutable.py
+++ src/allmydata/storage/immutable.py
@@ -6,8 +6,6 @@ from zope.interface import implements
 from allmydata.interfaces import RIBucketWriter, RIBucketReader
 from allmydata.util import base32, fileutil, log
 from allmydata.util.assertutil import precondition
-from allmydata.util.hashutil import constant_time_compare
-from allmydata.storage.lease import LeaseInfo
 from allmydata.storage.common import UnknownImmutableContainerVersionError, \
      DataTooLargeError
 
@@ -19,7 +17,7 @@ from allmydata.storage.common import UnknownImmutableContainerVersionError, \
 # The share file has the following layout:
 #  0x00: share file version number, four bytes, current version is 1
 #  0x04: share data length, four bytes big-endian = A # See Footnote 1 below.
-#  0x08: number of leases, four bytes big-endian
+#  0x08: number of leases, four bytes big-endian # Footnote[2]
 #  0x0c: beginning of share data (see immutable.layout.WriteBucketProxy)
 #  A+0x0c = B: first lease. Lease format is:
 #   B+0x00: owner number, 4 bytes big-endian, 0 is reserved for no-owner
@@ -36,8 +34,14 @@ from allmydata.storage.common import UnknownImmutableContainerVersionError, \
 # then the value stored in this field will be the actual share data length
 # modulo 2**32.
 
+# Footnote 2: as of Tahoe v(post-Accounting) this field is not used by
+# storage servers. New shares will have a 0 here. Old shares will have
+# whatever value was left over when the server was upgraded. All lease
+# information is now kept in the leasedb, managed by accounting.py
+
+# try to match new-s3 code: it computes the end-of-share at start, and then
+# forgets about leases and only remembers end-of-share
 class ShareFile:
-    LEASE_SIZE = struct.calcsize(">L32s32sL")
     sharetype = "immutable"
 
     def __init__(self, filename, max_size=None, create=False):
@@ -62,19 +66,14 @@ class ShareFile:
             # clients to read the first part of the share.
             f.write(struct.pack(">LLL", 1, min(2**32-1, max_size), 0))
             f.close()
-            self._lease_offset = max_size + 0x0c
-            self._num_leases = 0
         else:
             f = open(self.home, 'rb')
-            filesize = os.path.getsize(self.home)
-            (version, unused, num_leases) = struct.unpack(">LLL", f.read(0xc))
+            (version, unused) = struct.unpack(">LL", f.read(0x8))
             f.close()
             if version != 1:
                 msg = "sharefile %s had version %d but we wanted 1" % \
                       (filename, version)
                 raise UnknownImmutableContainerVersionError(msg)
-            self._num_leases = num_leases
-            self._lease_offset = filesize - (num_leases * self.LEASE_SIZE)
         self._data_offset = 0xc
 
     def unlink(self):
@@ -83,14 +82,14 @@ class ShareFile:
     def read_share_data(self, offset, length):
         precondition(offset >= 0)
         # reads beyond the end of the data are truncated. Reads that start
-        # beyond the end of the data return an empty string.
+        # beyond the end of the data return an empty string. Shares that have
+        # vestigal leases on the end of the file will allow a bit of
+        # overread, since we're no longer keeping track of how much leftover
+        # data is present.
         seekpos = self._data_offset+offset
-        actuallength = max(0, min(length, self._lease_offset-seekpos))
-        if actuallength == 0:
-            return ""
         f = open(self.home, 'rb')
         f.seek(seekpos)
-        return f.read(actuallength)
+        return f.read(length)
 
     def write_share_data(self, offset, data):
         length = len(data)
@@ -103,95 +102,6 @@ class ShareFile:
         assert f.tell() == real_offset
         f.write(data)
         f.close()
-
-    def _write_lease_record(self, f, lease_number, lease_info):
-        offset = self._lease_offset + lease_number * self.LEASE_SIZE
-        f.seek(offset)
-        assert f.tell() == offset
-        f.write(lease_info.to_immutable_data())
-
-    def _read_num_leases(self, f):
-        f.seek(0x08)
-        (num_leases,) = struct.unpack(">L", f.read(4))
-        return num_leases
-
-    def _write_num_leases(self, f, num_leases):
-        f.seek(0x08)
-        f.write(struct.pack(">L", num_leases))
-
-    def _truncate_leases(self, f, num_leases):
-        f.truncate(self._lease_offset + num_leases * self.LEASE_SIZE)
-
-    def get_leases(self):
-        """Yields a LeaseInfo instance for all leases."""
-        f = open(self.home, 'rb')
-        (version, unused, num_leases) = struct.unpack(">LLL", f.read(0xc))
-        f.seek(self._lease_offset)
-        for i in range(num_leases):
-            data = f.read(self.LEASE_SIZE)
-            if data:
-                yield LeaseInfo().from_immutable_data(data)
-
-    def add_lease(self, lease_info):
-        f = open(self.home, 'rb+')
-        num_leases = self._read_num_leases(f)
-        self._write_lease_record(f, num_leases, lease_info)
-        self._write_num_leases(f, num_leases+1)
-        f.close()
-
-    def renew_lease(self, renew_secret, new_expire_time):
-        for i,lease in enumerate(self.get_leases()):
-            if constant_time_compare(lease.renew_secret, renew_secret):
-                # yup. See if we need to update the owner time.
-                if new_expire_time > lease.expiration_time:
-                    # yes
-                    lease.expiration_time = new_expire_time
-                    f = open(self.home, 'rb+')
-                    self._write_lease_record(f, i, lease)
-                    f.close()
-                return
-        raise IndexError("unable to renew non-existent lease")
-
-    def add_or_renew_lease(self, lease_info):
-        try:
-            self.renew_lease(lease_info.renew_secret,
-                             lease_info.expiration_time)
-        except IndexError:
-            self.add_lease(lease_info)
-
-
-    def cancel_lease(self, cancel_secret):
-        """Remove a lease with the given cancel_secret. If the last lease is
-        cancelled, the file will be removed. Return the number of bytes that
-        were freed (by truncating the list of leases, and possibly by
-        deleting the file. Raise IndexError if there was no lease with the
-        given cancel_secret.
-        """
-
-        leases = list(self.get_leases())
-        num_leases_removed = 0
-        for i,lease in enumerate(leases):
-            if constant_time_compare(lease.cancel_secret, cancel_secret):
-                leases[i] = None
-                num_leases_removed += 1
-        if not num_leases_removed:
-            raise IndexError("unable to find matching lease to cancel")
-        if num_leases_removed:
-            # pack and write out the remaining leases. We write these out in
-            # the same order as they were added, so that if we crash while
-            # doing this, we won't lose any non-cancelled leases.
-            leases = [l for l in leases if l] # remove the cancelled leases
-            f = open(self.home, 'rb+')
-            for i,lease in enumerate(leases):
-                self._write_lease_record(f, i, lease)
-            self._write_num_leases(f, len(leases))
-            self._truncate_leases(f, len(leases))
-            f.close()
-        space_freed = self.LEASE_SIZE * num_leases_removed
-        if not len(leases):
-            space_freed += os.stat(self.home)[stat.ST_SIZE]
-            self.unlink()
-        return space_freed
 
 
 class BucketWriter(Referenceable):
