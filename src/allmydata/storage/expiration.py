1ef03cda42158e20b5d33905674e06a34101249c
David-Sarah Hopwood <david-sarah@jacaranda.org>
2012-09-20 20:26:34 +0100

    Add storage/expiration.py. This moves logic related to the expiration policy and parameters out of the server, and will be used by the accounting crawler (but is not used yet).
    Signed-off-by: David-Sarah Hopwood <david-sarah@jacaranda.org>

---
+++ src/allmydata/storage/expiration.py
@@ -0,0 +1,68 @@
+
+import time
+from types import NoneType
+
+from allmydata.util.assertutil import precondition
+from allmydata.util import time_format
+from allmydata.web.common import abbreviate_time
+
+
+class ExpirationPolicy(object):
+    def __init__(self, enabled=False, mode="age", override_lease_duration=None,
+                 cutoff_date=None, sharetypes=("mutable", "immutable")):
+        precondition(isinstance(enabled, bool), enabled=enabled)
+        precondition(mode in ("age", "cutoff-date"),
+                     "GC mode %r must be 'age' or 'cutoff-date'" % (mode,))
+        precondition(isinstance(override_lease_duration, (int, NoneType)),
+                     override_lease_duration=override_lease_duration)
+        precondition(isinstance(cutoff_date, int) or (mode != "cutoff-date" and cutoff_date is None),
+                     cutoff_date=cutoff_date)
+        precondition(isinstance(sharetypes, tuple), sharetypes=sharetypes)
+
+        self._mode = mode
+        self._override_lease_duration = override_lease_duration
+        self._cutoff_date = cutoff_date
+        if enabled:
+            self._sharetypes_to_expire = sharetypes
+        else:
+            self._sharetypes_to_expire = ()
+
+    def should_expire(self, current_time, renewal_time, expiration_time, sharetype):
+        if sharetype not in self._sharetypes_to_expire:
+            return False
+
+        if self._mode == "age":
+            if self._override_lease_duration is None:
+                expiry_time = expiration_time  # from lease
+            else:
+                expiry_time = renewal_time + self._override_lease_duration
+        else:
+            expiry_time = self._cutoff_date
+
+        return current_time >= expiry_time
+
+    def describe_enabled(self):
+        if self._sharetypes_to_expire:
+            return "Enabled: expired leases will be removed"
+        else:
+            return "Disabled: scan-only mode, no leases will be removed"
+
+    def describe_expiration(self):
+        if self._mode == "age":
+            if self._override_lease_duration is None:
+                return ("Leases will expire naturally, probably 31 days after "
+                        "creation or renewal.")
+            else:
+                return ("Leases created or last renewed more than %s ago "
+                        "will be considered expired."
+                        % abbreviate_time(self._override_lease_duration))
+        else:
+            localizedutcdate = time.strftime("%d-%b-%Y", time.gmtime(self._cutoff_date))
+            isoutcdate = time_format.iso_utc_date(self._cutoff_date)
+            return ("Leases created or last renewed before %s (%s) UTC "
+                    "will be considered expired." % (isoutcdate, localizedutcdate))
+
+    def describe_sharetypes(self):
+        return (" The following sharetypes will be expired: ",
+                " ".join(sorted(self._sharetypes_to_expire)), ".")
+
